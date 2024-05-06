86fb179d59e5d64c2559582efd829d6803bdb8f6
miguel <miguel.perezandrade@gmail.com>
2024-05-06 18:22:08 -0300

    function for incrementing balance

--- operations.py
+++ operations.py
@@ -2,6 +2,18 @@ import pandas as pd
 from custom_types import *
 from currency_conversion import *
 
+def increment_balance_ARS(balance, ars, date):
+    usd_price = get_ARS_to_USD(date)
+    usd = ars/usd_price
+    new_amount_ARS = balance.ARS+ars
+    new_amount_USD = balance.USD+usd
+    next_balance = Balance(ARS=new_amount_ARS, USD=new_amount_USD)
+
+    print(f"On {date} USD price: {usd_price}, ARS: {ars}, USD: {usd}")
+
+    return next_balance
+
+
 def deposit_operation(row: pd.DataFrame, current_state: State) -> State:
 
     deposit_operations = (
@@ -15,14 +27,7 @@ def deposit_operation(row: pd.DataFrame, current_state: State) -> State:
 
     operation = row['Operacion']
 
-    amount_ARS = row['Monto']*(1 if operation in deposit_operations else -1)
-    USD_price = get_ARS_to_USD(row['Fecha'])
-    amount_USD = amount_ARS/USD_price
-    new_amount_ARS = current_state.balance.ARS+amount_ARS
-    new_amount_USD = current_state.balance.USD+amount_USD
-    next_balance = Balance(ARS=new_amount_ARS, USD=new_amount_USD)
+    next_balance = increment_balance_ARS(current_state.balance, row['Monto'], row['Fecha'])
     next_state = State(balance=next_balance)
-
-    print(f"On {row['Fecha']} USD price: {USD_price}, ARS: {amount_ARS}, USD: {amount_USD}")
 
     return next_state  # Placeholder, replace with your actual logic
